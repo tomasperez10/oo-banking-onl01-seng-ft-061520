d3211ec92edb02605536c422ff5d34557bb63cc8
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 03:26:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @stauts == "pending"
+    if @sender.balance > @amount && @stauts = "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
