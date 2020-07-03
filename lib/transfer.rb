bbdeda5327d8a73edb251bbe1f16f97ca31ca4c1
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 03:28:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     end
-    @status = "rejected"
+    self.status = "rejected"
     return "Transaction rejected. Please check your account balance."
   end
 
