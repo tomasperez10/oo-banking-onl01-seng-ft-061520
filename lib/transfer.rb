eb30410dcb90b0bed4125cd4689e01d5b4cc4578
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 03:08:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,10 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
-    else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
+    else
+     @status = "complete"
     end
   end
 
